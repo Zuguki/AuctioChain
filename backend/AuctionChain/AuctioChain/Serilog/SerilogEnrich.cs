47320dd17142c010357c766a67e1178391f46f76
Zakhar Guskov <guskov.zahar20@gmail.com>
2023-10-23 13:18:07 +0500

    Serilog was added

---
+++ backend/AuctionChain/AuctioChain/Serilog/SerilogEnrich.cs
@@ -0,0 +1,13 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace AuctioChain.Serilog;
+
+public class SerilogEnrich : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("EventId", "AuctioChain"));
+        logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("User", "GoodGod"));
+    }
+}
---
+++ backend/AuctionChain/AuctioChain/Serilog/SerilogSetting.cs
@@ -0,0 +1,23 @@
+﻿using Destructurama;
+using Serilog;
+using Serilog.Events;
+
+namespace AuctioChain.Serilog;
+
+public static class SerilogSetting
+{
+    public static void Connect(HostBuilderContext context, LoggerConfiguration configuration)
+    {
+        configuration
+            .Enrich.WithThreadId()
+            .Enrich.FromLogContext()
+            .AuditTo.Sink<SerilogSink>()
+            .Filter.With<SerilogFilter>()
+            .Enrich.With<SerilogEnrich>()
+            .Destructure.UsingAttributes()
+            .WriteTo.Console(
+                LogEventLevel.Information,
+                outputTemplate:
+                "{Timestamp:HH:mm:ss:ms} LEVEL: [{Level}] THREAD: |{ThreadId}| USER: |{User}| ->{EventId}<- {Message}{NewLine}{Exception}");
+    }
+}

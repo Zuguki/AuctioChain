95dd93187f3109c5687346f825bebbbba69c0249
vlad1sat <pyatkov200316@mail.ru>
2023-10-27 11:47:11 +0500

    add font and styles for h

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
+    return (
+        <>
+            <h1>h1</h1>
+            <h2>h2</h2>
+            <h3>h3</h3>
+            <h4>h4</h4>
+            <h5>h5</h5>
+            <h6>h6</h6>
+            <p>dsds</p>
+            <h1> ddssdds</h1>
 
+        </>
+    )
 }
 
 export default App

f6d6de9386dba1c5c84ef84c1fefaefec79223f6
vlad1sat <pyatkov200316@mail.ru>
2023-11-01 23:00:59 +0500

    create hoook use tag

--- frontend/src/components/UI/Tag/Tag.tsx
+++ frontend/src/components/UI/Tag/Tag.tsx
@@ -1,31 +1,25 @@
-import {FC, useEffect, useState} from 'react';
-import tagStyle from './tag.module.css'
+import {FC} from 'react';
+import useTag from "./useTag.ts";
 
 interface ITag {
     children: string,
     isClick?: boolean,
 
 }
+
+
 const Tag: FC<ITag> = ({ children, isClick = false }) => {
     const textTag = '#' + children.replace(/\s+/g, '_');
 
     // hook
-    const [style, setStyle] = useState<string>(tagStyle.tag);
-    const [onActive, setOnActive] = useState<boolean>(false);
-    useEffect(() => {
-        if (onActive) {
-            setStyle((prevStyle: string): string => `${prevStyle} ${tagStyle.activeTag}`);
-            return;
-        }
-        setStyle((): string => tagStyle.tag);
-    }, [onActive]);
+    const {style, changeActive} = useTag();
 
     return (
         <div
             className={style} onClick={() => {
                 if (!isClick) return;
                 console.log('style')
-                setOnActive((prevOnActive: boolean) => !prevOnActive);
+                changeActive();
             }}>
             {textTag}
         </div>

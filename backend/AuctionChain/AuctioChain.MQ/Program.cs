31ae821760b2095f07956b0daa9e4d85d33652a0
Zakhar Guskov <guskov.zahar20@gmail.com>
2024-05-10 14:14:10 +0500

    add: massTransit

--- backend/AuctionChain/AuctioChain.MQ/Program.cs
+++ backend/AuctionChain/AuctioChain.MQ/Program.cs
@@ -1,5 +1,4 @@
 using AuctioChain.MQ.Publishers;
-using AuctioChain.MQ.Services;
 using AuctioChain.MQ.Services.Dto;
 using RabbitMQ.Client;
 
@@ -10,8 +9,8 @@ builder.Services.AddSingleton<IConnectionFactory>(_ => new ConnectionFactory
     Endpoint = new AmqpTcpEndpoint(),
     DispatchConsumersAsync = true,
 });
-builder.Services.AddHostedService<BlockchainBalanceListener>();
-builder.Services.AddHostedService<AuctionEndListener>();
+// builder.Services.AddHostedService<BlockchainBalanceListener>();
+// builder.Services.AddHostedService<AuctionEndListener>();
 
 builder.Services.AddSingleton<IPublisher<TransactionDto>, BlockchainPublisher>();
 builder.Services.AddSingleton<IPublisher<AuctionEndDto>, AuctionEndPublisher>();
